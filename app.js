614d81fabc8908b70682d3590458d4b9827830f3
kimi641 <liushiyi1991@gmail.com>
2017-06-16 21:46:25 +0800

    update

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
+const mount = require('koa-mount')
+const static = require('koa-static')
+const path = require('path')
 
 const index = require('./routes/index')
 const users = require('./routes/users')
@@ -18,10 +21,11 @@ app.use(bodyparser({
 }))
 app.use(json())
 app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
+app.use(static(path.join(__dirname,'./public')))
+//app.use(static(path.join(__dirname,'./node_modules')))
 
-app.use(views(__dirname + '/views', {
-  extension: 'pug'
+app.use(views(__dirname + '/public', {
+  extension: 'ejs'
 }))
 
 // logger
@@ -31,9 +35,15 @@ app.use(async (ctx, next) => {
   const ms = new Date() - start
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
+/*
+app.use(async(ctx) =>{
+    ctx.body = 'hello world'
+})
+*/
 
 // routes
 app.use(index.routes(), index.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
+//app.use(users.routes(), users.allowedMethods())
 
+app.listen(3001);
 module.exports = app
